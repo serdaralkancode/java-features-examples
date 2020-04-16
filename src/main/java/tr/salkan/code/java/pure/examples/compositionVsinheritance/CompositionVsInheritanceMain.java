df6396ebb2566239aa8939d4a253dc2767105d67
Serdar ALKAN <serdaralkan_@hotmail.com>
2020-04-16 15:05:35 +0300

    Composition VS Inheritance

---
+++ src/main/java/tr/salkan/code/java/pure/examples/compositionVsinheritance/CompositionVsInheritanceMain.java
@@ -0,0 +1,8 @@
+package tr.salkan.code.java.pure.examples.compositionVsinheritance;
+
+public class CompositionVsInheritanceMain {
+
+    public static void main(String[] args) {
+        
+    }
+}
